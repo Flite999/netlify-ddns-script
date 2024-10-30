b56a58463242b4d21ee4b8b4c5aa649997ab7850
Mark <17260591+MarkKleinhaus@users.noreply.github.com>
2024-10-30 16:53:35 -0700

    update some logic

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -16,6 +17,12 @@ import (
 type Config struct {
 	AccessToken string
 	ZoneId      string
+	Hostname    string
+}
+
+type DNSRecord struct {
+	Id    string `json:"id"`
+	Value string `json:"value"`
 }
 
 func loadConfig() (Config, error) {
@@ -26,6 +33,7 @@ func loadConfig() (Config, error) {
 	}
 	config.AccessToken = os.Getenv("ACCESS_TOKEN")
 	config.ZoneId = os.Getenv("ZONE_ID")
+	config.Hostname = os.Getenv("HOSTNAME")
 	return config, nil
 }
 
@@ -42,7 +50,12 @@ func main() {
 	for {
 		ipAdd := checkIP()
 		if ipAdd != prevIp {
-			updateDNS(config, ipAdd)
+			// Update DNS record - delete previous record and create new record since there is no update option
+			records := getDNSRecords(config, prevIp)
+			deleteDNSRecord(config, records)
+			createDNSRecord(config, ipAdd)
+
+			fmt.Println("DNS record updated successfully")
 			prevIp = ipAdd
 		}
 
@@ -65,7 +78,7 @@ func checkIP() string {
 	return string(output)
 }
 
-func getDNSRecords(config Config) {
+func getDNSRecords(config Config, prevIP string) []string {
 	// get all DNS records for zone id
 	url := fmt.Sprintf("https://api.netlify.com/api/v1/dns_zones/%s/dns_records", config.ZoneId)
 
@@ -93,7 +106,56 @@ func getDNSRecords(config Config) {
 		log.Fatalf("Failed to get DNS record: %s", body)
 	}
 
-	fmt.Println("DNS records:", string(body))
+	var records []DNSRecord
+	err = json.Unmarshal(body, &records)
+	if err != nil {
+		log.Fatalf("Failed to parse DNS records: %v", err)
+	}
+
+	var matchingRecordIds []string
+	for _, record := range records {
+		if record.Value == prevIP {
+			matchingRecordIds = append(matchingRecordIds, record.Id)
+		}
+	}
+
+	return matchingRecordIds
+}
+
+func deleteDNSRecord(config Config, records []string) {
+	if len(records) == 0 {
+		fmt.Println("No DNS records to delete")
+		return
+	}
+	for _, recordId := range records {
+		// delete a DNS record for zone id
+		url := fmt.Sprintf("https://api.netlify.com/api/v1/dns_zones/%s/dns_records/%s", config.ZoneId, recordId)
+		req, err := http.NewRequest("DELETE", url, nil)
+		if err != nil {
+			log.Fatalf("Failed to create HTTP request: %v", err)
+		}
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", "Bearer "+config.AccessToken)
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatalf("Failed to execute HTTP request: %v", err)
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalf("Failed to read HTTP response: %v", err)
+		}
+
+		if resp.StatusCode != http.StatusNoContent {
+			log.Fatalf("Failed to delete DNS record: %s", body)
+		}
+
+		fmt.Println("DNS record deleted successfully")
+	}
 }
 
 func createDNSRecord(config Config, ipAdd string) {
@@ -109,10 +171,10 @@ func createDNSRecord(config Config, ipAdd string) {
 	//@ will automatically set hostname to zone id hostname
 	reqBody := fmt.Sprintf(`{
 		"type": "A",
-		"hostname": "fliteconsulting.io",
+		"hostname": "%s",
 		"value": "%s",
 		"ttl": 3600
-	}`, ipAdd)
+	}`, config.Hostname, ipAdd)
 
 	req.Body = io.NopCloser(strings.NewReader(reqBody))
 
@@ -133,12 +195,4 @@ func createDNSRecord(config Config, ipAdd string) {
 	}
 
 	fmt.Println("DNS record created successfully")
-}
-
-func updateDNS(config Config, ipAdd string) {
-	getDNSRecords(config)
-
-	createDNSRecord(config, ipAdd)
-
-	fmt.Println("DNS record updated successfully")
 }
